845f8fea7bc3ced065aeeb4bcff2120b6a535981
swift <officialswiftstand@gmail.com>
2025-06-02 23:39:22 +0100

    some more adjs

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
-    sass = require('gulp-ruby-sass'),
+    sass = require('gulp-sass')(require('sass')), // Use Dart Sass instead of Ruby Sass
     autoprefixer = require('gulp-autoprefixer'),
     browserSync = require('browser-sync').create();
 
@@ -21,21 +21,22 @@ gulp.task('scripts', function() {
       .pipe(browserSync.stream());
 });
 
-// TODO: Maybe we can simplify how sass compile the minify and unminify version
-var compileSASS = function (filename, options) {
-  return sass('src/scss/*.scss', options)
+gulp.task('sass', function() {
+    return gulp.src('src/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer('last 2 versions', '> 5%'))
-        .pipe(concat(filename))
+        .pipe(concat('custom.css'))
         .pipe(gulp.dest(DEST+'/css'))
         .pipe(browserSync.stream());
-};
-
-gulp.task('sass', function() {
-    return compileSASS('custom.css', {});
 });
 
 gulp.task('sass-minify', function() {
-    return compileSASS('custom.min.css', {style: 'compressed'});
+    return gulp.src('src/scss/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(autoprefixer('last 2 versions', '> 5%'))
+        .pipe(concat('custom.min.css'))
+        .pipe(gulp.dest(DEST+'/css'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('browser-sync', function() {
@@ -51,12 +52,13 @@ gulp.task('watch', function() {
   // Watch .html files
   gulp.watch('production/*.html', browserSync.reload);
   // Watch .js files
-  gulp.watch('src/js/*.js', ['scripts']);
+  gulp.watch('src/js/*.js', gulp.series('scripts'));
   // Watch .scss files
-  gulp.watch('src/scss/*.scss', ['sass', 'sass-minify']);
+  gulp.watch('src/scss/*.scss', gulp.series('sass', 'sass-minify'));
 });
+
+// Build task for production
+gulp.task('build', gulp.series('scripts', 'sass', 'sass-minify'));
 
 // Default Task
 gulp.task('default', gulp.series('browser-sync', 'watch'));
-
-gulp.task('build', gulp.series('scripts', 'sass', 'sass-minify'));
